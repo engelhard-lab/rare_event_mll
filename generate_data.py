962f1694551e38b313a80807c8fb829be29d9fac
Matthew M Engelhard <mme@Matthews-iMac.attlocal.net>
2023-04-17 17:50:09 -0400

    added generate data with shared features

--- generate_data.py
+++ generate_data.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 N_PATIENTS = 10000
-N_FEATURES = 100
+N_FEATURES = 10
 RANDOM_SEED = 2023
-STEP_SIZE = 1e-3
+STEP_SIZE = 1e-2
 
 EVENT_RATE = 0.01
 SIMILARITY = 0.95
@@ -11,7 +11,14 @@ SIMILARITY = 0.95
 
 def main():
 
-	x, e1, e2 = generate_data(plot=True)
+	x, e1, e2 = generate_data_linear(plot=True)
+
+	print('Rate of event 1: %.3f' % np.mean(e1))
+	print('Rate of event 2: %.3f' % np.mean(e2))
+	print('Rate of co-occurrence: %.3f' % np.mean(e1 & e2))
+	print('Correlation between events: %.3f' % np.corrcoef(e1, e2)[0, 1])
+
+	x, e1, e2 = generate_data_shared_features(1, 5, shared_second_layer_weights=True, plot=True)
 
 	print('Rate of event 1: %.3f' % np.mean(e1))
 	print('Rate of event 2: %.3f' % np.mean(e2))
@@ -19,22 +26,97 @@ def main():
 	print('Correlation between events: %.3f' % np.corrcoef(e1, e2)[0, 1])
 
 
-def generate_data(plot=False):
+def generate_data_shared_features(
+	n_distinct, n_overlapping,
+	n_patients=N_PATIENTS, n_features=N_FEATURES,
+	random_seed=RANDOM_SEED, event_rate=EVENT_RATE,
+	step_size=STEP_SIZE, shared_second_layer_weights=True,
+	plot=False):
 
-	rs = np.random.RandomState(RANDOM_SEED)
+	rs = np.random.RandomState(random_seed)
 
 	# generate N_FEATURES-dimensional feature vector for N_PATIENTS
-	x = rs.randn(N_PATIENTS, N_FEATURES)
+	x = rs.randn(n_patients, n_features)
+
+	n_random_features = n_overlapping + 2 * n_distinct
+
+	# generate coefficient matrix defining random features
+	#W = rs.randn(n_features, n_random_features)
+	W = glorot_uniform(rs, n_features, n_random_features)
+
+	h = sigmoid(x @ W)
+
+	# generate coefficient vector for second layer
+	c1 = glorot_uniform(rs, n_random_features, 1)
+
+	if shared_second_layer_weights:
+		c2 = c1.copy() # copying here is critical otherwise it's the same object
+	else:
+		c2 = glorot_uniform(rs, n_random_features, 1)
+
+	# zero coefficients such that outcomes 1 and 2 depend on 
+	# a) n_overlapping overlapping features; and
+	# b) n_distinct distinct features
+	c1[:n_distinct] = 0
+	c2[n_distinct: (2 * n_distinct)] = 0
+
+	# print similarity between (normed) c1 and c2
+	print('Dot product of u1 and u2: %.2f' % np.dot(normalize(c1), normalize(c2)))
+
+	# find logit offset that gives the desired event rate
+	offset1 = find_offset(
+		rs,
+		np.dot(h, c1),
+		event_rate,
+		step_size
+	)
+
+	offset2 = find_offset(
+		rs,
+		np.dot(h, c2),
+		event_rate,
+		step_size
+	)
+
+	# calculate logits for each event
+	l1 = np.dot(h, c1) - offset1
+	l2 = np.dot(h, c2) - offset2
+
+	# calculate probability of each event
+	p1 = sigmoid(l1)
+	p2 = sigmoid(l2)
+
+	# generate events
+	e1 = bernoulli_draw(rs, p1)
+	e2 = bernoulli_draw(rs, p2)
+
+	if plot:
+
+		plot_logits_and_probs(l1, l2, p1, p2)
+
+	return x, e1, e2
+
+
+def generate_data_linear(
+	n_patients=N_PATIENTS, n_features=N_FEATURES,
+	random_seed=RANDOM_SEED, event_rate=EVENT_RATE,
+	step_size=STEP_SIZE, similarity=SIMILARITY,
+	plot=False):
+
+	rs = np.random.RandomState(random_seed)
+
+	# generate n_features-dimensional feature vector for N_PATIENTS
+	x = rs.randn(n_patients, n_features)
 
 	# generate coefficient vectors for events 1 and 2
-	u1, u2 = generate_vectors_by_similarity(rs, N_FEATURES, SIMILARITY)
+	u1, u2 = generate_vectors_by_similarity(rs, n_features, similarity)
 
 	# find logit offset that gives the desired event rate
 	offset = find_offset(
 		rs,
-		np.dot(x, normed_uniform(rs, N_FEATURES)),
-		EVENT_RATE,
-		STEP_SIZE
+		np.dot(x, normed_uniform(rs, n_features)),
+		event_rate,
+		step_size
 	)
 
 	# print similarity between u1 and u2
@@ -54,23 +136,28 @@ def generate_data(plot=False):
 
 	if plot:
 
-		import matplotlib.pyplot as plt
-
-		fig, ax = plt.subplots(ncols=2, nrows=1, figsize=(10, 4))
-
-		ax[0].hist(l1, alpha=.5, bins=20, label='Event 1')
-		ax[0].hist(l2, alpha=.5, bins=20, label='Event 2')
-		ax[0].set_title('Event Logits')
-		ax[0].legend()
-
-		ax[1].hist(p1, alpha=.5, bins=20, label='Event 1')
-		ax[1].hist(p2, alpha=.5, bins=20, label='Event 2')
-		ax[1].set_title('Event Probabilities')
-		ax[1].legend()
-
-		plt.show()
+		plot_logits_and_probs(l1, l2, p1, p2)
 
 	return x, e1, e2
+
+
+def plot_logits_and_probs(l1, l2, p1, p2):
+
+	import matplotlib.pyplot as plt
+
+	fig, ax = plt.subplots(ncols=2, nrows=1, figsize=(10, 4))
+
+	ax[0].hist(l1, alpha=.5, bins=20, label='Event 1')
+	ax[0].hist(l2, alpha=.5, bins=20, label='Event 2')
+	ax[0].set_title('Event Logits')
+	ax[0].legend()
+
+	ax[1].hist(p1, alpha=.5, bins=20, label='Event 1')
+	ax[1].hist(p2, alpha=.5, bins=20, label='Event 2')
+	ax[1].set_title('Event Probabilities')
+	ax[1].legend()
+
+	plt.show()
 
 
 def generate_vectors_by_similarity(rs, n, s):
@@ -125,6 +212,10 @@ def sigmoid(l):
 
 def normalize(v):
 	return v / np.linalg.norm(v)
+
+
+def relu(v):
+	return np.maximum(v, 0)
 
 
 if __name__ == '__main__':
