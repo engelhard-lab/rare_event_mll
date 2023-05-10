6b711b6a71c68969385fc94c49102e26e2ce81cc
Qin Weng <qw115@duke.edu>
2023-05-10 12:09:08 -0400

    Results for early stopping and decrease er

--- Results/plot_shared_features_results.py
+++ Results/plot_shared_features_results.py
@@ -1,12 +1,21 @@
 from Results.base_plot_auc_ap import plot_auc_ap
 
-file_name = 'sklearn/test_rs.csv'
+file_name = 'torch/newER_full_overlap.csv'
+
 vars = {
     'x_var': 'hidden_layers',
     'hue_var': 'method',
     'row_var': 'metric',
     'col_var': 'activation'
 }
-calc_vars = {'% overlap': '(1-n_distinct/n_random_features)'}
+calc_vars = {}
+
+# vars = {
+#     'x_var': '% overlap',
+#     'hue_var': 'method',
+#     'row_var': 'metric',
+#     'col_var': 'activation'
+# }
+# calc_vars = {'% overlap': '(1-n_distinct/n_random_features)'}
 
 plot_auc_ap(file_name=file_name, vars=vars, calc_vars=calc_vars)
