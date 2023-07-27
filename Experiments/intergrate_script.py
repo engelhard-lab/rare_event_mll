8ee9956c8e0c0f75c0752bd0dcbc7c20ab8b73db
Qin <118118177+threeefingers@users.noreply.github.com>
2023-07-27 12:14:15 -0400

    Update intergrate_script.py

--- Experiments/intergrate_script.py
+++ Experiments/intergrate_script.py
@@ -1,12 +1,11 @@
-from generate_data import generate_data_linear, generate_data_shared_features
-from Models.torch.torch_training import torch_classifier
-
 import itertools
 import pandas as pd
 import numpy as np
 from sklearn.metrics import roc_auc_score, average_precision_score
 from sklearn.model_selection import train_test_split
 import time
+from generate_data import generate_data_shared_features
+from Models.torch.torch_training import torch_classifier
 
 """NOTE: the CWD needs to be set to the base directory. And you need to add
 the the path to rare_event_mll/ to your PYTHONPATH."""
@@ -15,39 +14,71 @@ the the path to rare_event_mll/ to your PYTHONPATH."""
 and they will all be run. be aware when combining multiple lists that this
 can lead to very long run time to complete all combinations."""
 
-save_file = 'torch/final_test.csv'  # saved inside Results/ folder
+# change similarity
+save_file = 'torch/sim.csv'  # saved inside Results/ folder
 n_patients = 250000  # n of samples to generate
 n_features = 50  # n of features to generate
-event_rate = 0.001  # event rate for sample
+event_rate1 = [0.01]  # event rate for sample
 er2_ratio = [5]
 model_types = ['new_torch']  # options are 'sklearn' and 'torch'
 activations = ['relu']  # activation function. currently only support relu
 similarity_measures = {
-    'n_distinct': [0, 1, 2, 3, 4, 6, 8, 10],  # n of distinct features for each label
+    'n_distinct': [0, 2, 4, 6, 8, 10],  # n of distinct features for each label
     'n_random_features': [10],  # n of hidden features for each label
-    'shared_second_layer_weights': [True]  # whether the labels share the same weights of their features
+    'shared_second_layer_weights': [False]  # whether the labels share the same weights of their features
 }
 sim_keys, sim_values = zip(*similarity_measures.items())
 similarity_combos = [dict(zip(sim_keys, v)) for v in
                         itertools.product(*sim_values)]
+                        
+# # change event rate2 (ratio)
+# save_file = 'torch/final_ratio_test_scale.csv'  # saved inside Results/ folder
+# n_patients = 250000  # n of samples to generate
+# n_features = 50  # n of features to generate
+# event_rate1 = [0.01]  # event rate for sample
+# er2_ratio = [1,2,3,5,10,30]
+# model_types = ['new_torch']  # options are 'sklearn' and 'torch'
+# activations = ['relu']  # activation function. currently only support relu
+# similarity_measures = {
+#     'n_distinct': [1],  # n of distinct features for each label
+#     'n_random_features': [10],  # n of hidden features for each label
+#     'shared_second_layer_weights': [True]  # whether the labels share the same weights of their features
+# }
+# sim_keys, sim_values = zip(*similarity_measures.items())
+# similarity_combos = [dict(zip(sim_keys, v)) for v in
+#                         itertools.product(*sim_values)]
+
+# # change event rate1
+# save_file = 'torch/final_er1_test_scale.csv'  # saved inside Results/ folder
+# n_patients = 250000  # n of samples to generate
+# n_features = 50  # n of features to generate
+# event_rate1 = [0.001, 0.002, 0.005, 0.01, 0.02]  # event rate for sample
+# er2_ratio = [5]
+# model_types = ['new_torch']  # options are 'sklearn' and 'torch'
+# activations = ['relu']  # activation function. currently only support relu
+# similarity_measures = {
+#     'n_distinct': [1],  # n of distinct features for each label
+#     'n_random_features': [10],  # n of hidden features for each label
+#     'shared_second_layer_weights': [True]  # whether the labels share the same weights of their features
+# }
+# sim_keys, sim_values = zip(*similarity_measures.items())
+# similarity_combos = [dict(zip(sim_keys, v)) for v in
+#                         itertools.product(*sim_values)]
+
 
 param_config = {
-    "batch_size": [64, 256],
+    "batch_size": [64, 128, 256],
     "learning_rate": [1e-3, 1e-4],
     "regularization": [1e-5],
     "hidden_layers": [[200],[50],[10]],
 }
-# param_config = {
-#     "learning_rate": 1e-4,
-#     "regularization": 1e-6,
-#     "hidden_layers": [200]
-# }
 
-n_iters = 10  # n of iterations to run each combination
+
+n_iters = 20  # n of iterations to run each combination
 test_perc = 0.8  # percent of samples to use for test set
 print_time = True  # whether to print updates after each combination is completes
 print_output = True  # whether to print details about each generated dataset
-plot = True  # whether to plot details of each generated dataset
+plot = False  # whether to plot details of each generated dataset
 run_combined = False
 loss_plot = False  # whether to plot learning loss
 early_stop = True  # whether to do early stopping in training
@@ -55,93 +86,114 @@ early_stop = True  # whether to do early stopping in training
 
 columns = ['patients', 'features',
            'event_rate', 'er2_ratio',
-           'iter', 
-           'auc_single', 'ap_single',
-           'r2_single', 'cov_single', 
-           'auc_multi', 'ap_multi',
-           'r2_multi', 'cov_multi',
-           'config_single', 'config_multi']
+           'iter','config',
+           'auc', 'ap', 'r2', 'cov',
+           ]
 
 results = pd.DataFrame(columns=columns + list(similarity_measures.keys()))
 
 start = time.time()
-for s in similarity_combos:
-    for er2 in er2_ratio:
-            for r in range(n_iters):
-                datagen_args = {
-                    'n_patients': n_patients,
-                    'n_features': n_features,
-                    'event_rate1': event_rate,
-                    'event_rate2': er2*event_rate,
-                    'print_output': print_output,
-                    'plot': plot
+for event_rate in event_rate1:
+    single_first_time = True
+    for s in similarity_combos:
+        for ratio in er2_ratio:
+                for r in range(n_iters):
+                    datagen_args = {
+                        'n_patients': n_patients,
+                        'n_features': n_features,
+                        'event_rate1': event_rate,
+                        'event_rate2': ratio*event_rate,
+                        'print_output': print_output,
+                        'plot': plot
+                        }
+                    datagen_args['random_seed'] = r
+                    datagen_args.update(s)
+                    x, p1, e1, e2 = generate_data_shared_features(**datagen_args)
+                    x_train, x_test, \
+                    p1_train, p1_test, \
+                    e1_train, e1_test, \
+                    e2_train, e2_test = train_test_split(x, p1, e1, e2,
+                                                        random_state=r,
+                                                        test_size=test_perc,
+                                                        stratify=e1)
+    
+                    input_data = {
+                        "x_train": x_train,
+                        "x_test": x_test,
+                        "y_train": np.concatenate([e1_train.reshape(-1, 1),
+                                                    e2_train.reshape(-1, 1),
+                                                    p1_train.reshape(-1, 1)], axis=1),
+                        "y_test": np.concatenate([e1_test.reshape(-1, 1),
+                                                    e2_test.reshape(-1, 1),
+                                                    p1_test.reshape(-1, 1)], axis=1),
                     }
-                datagen_args['random_seed'] = r
-                datagen_args.update(s)
-                x, p1, e1, e2 = generate_data_shared_features(**datagen_args)
-                print(sum(e1))
-                e_combine = [str(label1)+str(label2) for label1, label2 in zip (e1, e2)]
-                x_train, x_test, \
-                p1_train, p1_test, \
-                e1_train, e1_test, \
-                e2_train, e2_test, \
-                _, _ = train_test_split(x, p1, e1, e2, e_combine,
-                                                    random_state=r,
-                                                    test_size=test_perc,
-                                                    stratify=e1)
-
-                input_data = {
-                    "x_train": x_train,
-                    "x_test": x_test,
-                    "y_train": np.concatenate([e1_train.reshape(-1, 1),
-                                                e2_train.reshape(-1, 1),
-                                                p1_train.reshape(-1, 1)], axis=1),
-                    "y_test": np.concatenate([e1_test.reshape(-1, 1),
-                                                e2_test.reshape(-1, 1),
-                                                p1_test.reshape(-1, 1)], axis=1),
-                }
-
-                best_perform_sin, best_perform_mul = 0, 0
-                best_config_sin, best_config_mul = {}, {}
-
-                for lr in param_config["learning_rate"]:
-                    for batch in param_config["batch_size"]:
-                        for h in param_config["hidden_layers"]:
-                            for lam in param_config["regularization"]:
-                                config = {
-                                    "learning_rate": lr,
-                                    "batch_size": batch,
-                                    "hidden_layers": h,
-                                    "regularization": lam,
-                                }
-                                performance = torch_classifier(config=config,
-                                                            input_data=input_data,
-                                                            valid=True,
-                                                            random_seed=r)
-                                print(performance)
-                                if performance[0]>best_perform_sin:
-                                    best_config_sin = config
-                                    best_perform_sin = performance[0]
-                                if performance[1]>best_perform_mul:
-                                    best_config_mul = config
-                                    best_perform_mul = performance[1]
+                    
+                    if single_first_time:
+                        best_perform_sin = 0
+                        best_config_sin = {}
+                        for lr in param_config["learning_rate"]:
+                            for batch in param_config["batch_size"]:
+                                for h in param_config["hidden_layers"]:
+                                    for lam in param_config["regularization"]:
+                                        config = {
+                                            "learning_rate": lr,
+                                            "batch_size": batch,
+                                            "hidden_layers": h,
+                                            "regularization": lam,
+                                        }
+                                        performance = torch_classifier(config=config,
+                                                                    input_data=input_data,
+                                                                    valid=True,
+                                                                    random_seed=r,
+                                                                    method="single")
+                                        if performance > best_perform_sin:
+                                            best_config_sin = config
+                                            best_perform_sin = performance
+                        
+                        perform_sin = torch_classifier(config=best_config_sin,
+                                                    input_data=input_data,
+                                                    valid=False,
+                                                    random_seed=r,
+                                                    method="single")
+                        results.loc[len(results.index)] = [n_patients, n_features, event_rate, 0, r] + \
+                                                          [str(best_config_sin)] + \
+                                                          perform_sin + \
+                                                          list(s.values())
+                        
+                    best_perform_mul = 0
+                    best_config_mul = {}
+                    for lr in param_config["learning_rate"]:
+                        for batch in param_config["batch_size"]:
+                            for h in param_config["hidden_layers"]:
+                                for lam in param_config["regularization"]:
+                                    config = {
+                                        "learning_rate": lr,
+                                        "batch_size": batch,
+                                        "hidden_layers": h,
+                                        "regularization": lam,
+                                    }
+                                    performance = torch_classifier(config=config,
+                                                                input_data=input_data,
+                                                                valid=True,
+                                                                random_seed=r,
+                                                                method="multi")
+                                    if performance>best_perform_mul:
+                                        best_config_mul = config
+                                        best_perform_mul = performance
+                    perform_mul = torch_classifier(config=best_config_mul,
+                                                input_data=input_data,
+                                                valid=False,
+                                                random_seed=r,
+                                                method="multi")
+                    results.loc[len(results.index)] = [n_patients, n_features, event_rate, ratio, r] + \
+                                                      [str(best_config_mul)] + \
+                                                      perform_mul + \
+                                                      list(s.values())
+                    results.to_csv("test2.csv")
                 
-                perform_sin = torch_classifier(config=best_config_sin,
-                                            input_data=input_data,
-                                            valid=False,
-                                            random_seed=r)
-                perform_mul = torch_classifier(config=best_config_mul,
-                                            input_data=input_data,
-                                            valid=False,
-                                            random_seed=r)
-                                
-                results.loc[len(results.index)] = [n_patients, n_features,
-                                                   event_rate, er2_ratio, r,] + \
-                                                perform_sin[:4] + perform_mul[4:] + \
-                                                [str(best_config_sin), str(best_config_mul)] + \
-                                                list(s.values())
-                
-                results.to_csv("final_er.csv")
+                single_first_time=False
+                   
+
 
 
 results.to_csv(f'Results/{save_file}', index=False)
